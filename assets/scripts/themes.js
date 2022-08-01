f9680f34e906961216837e10ecfee3c7204a08f3
brandogn <bwong928@berkeley.edu>
2022-08-01 13:50:57 -0700

    progress in layout - full pages

--- assets/scripts/themes.js
+++ assets/scripts/themes.js
@@ -4,6 +4,14 @@
 // theme will always have one rule, which is :root
 // with all the theme's color variables
 const themeSheet = document.styleSheets.item(1);
+const colors = {
+  blue: colorWay("#0b415a", "#dbb845"),
+  purp: colorWay("#342550", "#ff8b8b"),
+  red: colorWay("#442121", "#f5db4b"),
+  orange: colorWay("#30281d", "#a3ddb9"),
+  green: colorWay("#20311c", "#ca7e27"),
+  cream: colorWay("#292b2b", "#f0e2a2"),
+};
 
 function colorWay(bg, font) {
   return `:root {
@@ -17,12 +25,12 @@ function changeColor(rule) {
   themeSheet.insertRule(rule, 0);
 }
 
-// Color Ways:
+// ==================== DEBUG: ====================
+
+// Test Color Ways:
 const test = colorWay("#282828", "#ff0000");
 const test2 = colorWay("#43224a", "#ff002f");
 
 changeColor(test2);
-
-// ==================== DEBUG LOGS: ====================
 
 console.log(themeSheet.cssRules);
