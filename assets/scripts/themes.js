4d210f838b8e8aaf913cc3336900bd4e7557386d
brandogn <bwong928@berkeley.edu>
2022-09-01 09:44:26 -0700

    update theme

--- assets/scripts/themes.js
+++ assets/scripts/themes.js
@@ -5,19 +5,20 @@
 // with all the theme's color variables
 const themeSheet = document.styleSheets.item(1);
 const colors = {
-  default: `:root {
+	default: `:root {
     --bg-color: #020202;
-    --font-color: rgb(255, 247, 223);}`,
-  cobalt: colorWay("#0b415a", "#dbb845"),
-  purp: colorWay("#342550", "#ff8b8b"),
-  maroon: colorWay("#442121", "#f5db4b"),
-  orange: colorWay("#30281d", "#a3ddb9"),
-  green: colorWay("#20311c", "#ca7e27"),
-  cream: colorWay("#292b2b", "#f0e2a2"),
+    --font-color: rgb(255, 247, 223);
+  }`,
+	cobalt: colorWay("#0b415a", "#dbb845"),
+	purp: colorWay("#342550", "#ff8b8b"),
+	maroon: colorWay("#442121", "#f5db4b"),
+	orange: colorWay("#30281d", "#a3ddb9"),
+	green: colorWay("#20311c", "#ca7e27"),
+	cream: colorWay("#292b2b", "#f0e2a2"),
 };
 
 function colorWay(bg, font) {
-  return `:root {
+	return `:root {
         font-family: "MS-Font";
         --bg-color: ${bg};
         --font-color: ${font};
@@ -25,36 +26,43 @@ function colorWay(bg, font) {
 }
 
 function changeColor(rule) {
-  themeSheet.deleteRule(0);
-  themeSheet.insertRule(colors[rule], 0);
+	themeSheet.deleteRule(0);
+	themeSheet.insertRule(colors[rule], 0);
 }
 
 let toggled = false;
 function toggleColors() {
-  if (toggled) {
-    changeColor("default");
-  } else {
-    changeColor("cobalt");
-  }
-  document.querySelector(".color-selector").classList.toggle("hidden");
-  toggled = !toggled;
+	if (toggled) {
+		changeColor("default");
+	} else {
+		changeColor("cobalt");
+	}
+	document
+		.querySelector(".color-selector")
+		.classList.toggle("hidden");
+	toggled = !toggled;
 }
 
 let index = 1;
 function setRandomColor() {
-  const colorKeys = Object.keys(colors);
-  // const color = colorKeys[1 + Math.floor((colorKeys.length - 1) * Math.random())];
-  const color = colorKeys[1 + (index % (colorKeys.length - 1))];
-  console.log(colorKeys, color);
-  changeColor(color);
-  index++;
+	const colorKeys = Object.keys(colors);
+	// const color = colorKeys[1 + Math.floor((colorKeys.length - 1) * Math.random())];
+	const color =
+		colorKeys[1 + (index % (colorKeys.length - 1))];
+	console.log(colorKeys, color);
+	changeColor(color);
+	index++;
 }
 
 // ==================== DOM ====================
 
 document
-  .getElementById("color-btn")
-  .addEventListener("click", () => toggleColors());
+	.querySelectorAll("#color-btn")
+	.forEach((x) =>
+		x.addEventListener("click", () => toggleColors())
+	);
 document
-  .querySelector(".color-selector")
-  .addEventListener("click", setRandomColor);
+	.querySelector(".color-selector")
+	.addEventListener("click", setRandomColor);
+
+  toggleColors()
